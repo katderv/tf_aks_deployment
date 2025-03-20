441ffe6087abcb0e14743d51a9db55d0bb689a93
katderv <79928593+katderv@users.noreply.github.com>
2025-03-20 22:50:30 +0200

    typo fix

--- tf/main.tf
+++ tf/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = var.storage_account_name
     container_name       = var.container_name
-    be_resource_group_name  = var.resource_group_name
+    be_resource_group_name  = var.be_resource_group_name
     access_key           = var.access_key
   }
 }

a2384e6302a1d08996200e4ab1c1b60dbe410508
katderv <79928593+katderv@users.noreply.github.com>
2025-03-21 01:32:37 +0200

    test

--- tf/variables.tf
+++ tf/variables.tf
@@ -15,14 +15,18 @@ variable "resource_group_name" {
 
 variable "storage_account_name" {
   type        = string
+  default = null
 }
 variable "container_name" {
   type        = string
+  default = null
 }
 variable "access_key" {
   type        = string
+  default = null
 }
 
 variable "key" {
   type        = string
+  default = null
 }
